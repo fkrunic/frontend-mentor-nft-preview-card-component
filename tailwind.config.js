08d49134ace24e05e2439f7d60cf41277c84c24f
Filip Krunic <fkrunic9@gmail.com>
2023-09-01 12:17:59 -0700

    configured assets

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,23 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx,vue}",
   ],
   theme: {
+    fontFamily: {
+      outfit: 'Outfit, sans-serif'
+    },
+    screens: {
+      desktop: '1440px'
+    },
+    colors: {
+      'soft-blue': 'hsl(215, 51%, 70%)',
+      'cyan': 'hsl(178, 100%, 50%)',
+      'dark-blue-wallpaper': 'hsl(217, 54%, 11%)',
+      'dark-blue-card': 'hsl(216, 50%, 16%)',
+      'dark-blue-line': 'hsl(215, 32%, 27%)',
+      white: 'hsl(0, 0%, 100%)'
+    },
+    fontSize: {
+      regular: '18px'
+    },
     extend: {},
   },
   plugins: [],
