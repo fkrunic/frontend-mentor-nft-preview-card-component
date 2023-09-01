93ed8c10142d32cd1c17714416b1c6f3e975d61a
Filip Krunic <fkrunic9@gmail.com>
2023-09-01 12:55:33 -0700

    active states finished

--- src/App.vue
+++ src/App.vue
@@ -4,17 +4,27 @@
 <template>
   <div class="flex justify-center items-center min-h-screen">
 
-
+    <!-- Card -->
     <div id="card" class="flex flex-col gap-4 bg-dark-blue-card w-80 p-6 rounded-2xl shadow-xl shadow-cyan">
-      <img class="rounded-xl" src="./assets/img/image-equilibrium.jpg" alt="image-equilibrium">
+      <div class="relative">
+        <img class="rounded-xl" src="./assets/img/image-equilibrium.jpg" alt="image-equilibrium">
 
+        <!-- Hover overlay -->
+        <div class="opacity-0 hover:opacity-100 hover:cursor-pointer">
+          <div class="absolute w-full h-full bg-cyan/40 top-0 rounded-2xl"></div>
+          <img class="absolute top-28 left-28" src="./assets/img/icon-view.svg" alt="icon-view">
+        </div>
 
-      <p class="text-white font-semibold text-xl">Equilibrium #3429</p>
+      </div>
+
+      <!-- Description -->
+      <p class="text-white font-semibold text-xl hover:text-cyan hover:cursor-pointer">Equilibrium #3429</p>
       <p class="text-soft-blue font-light">
         Our Equilibrium collection promotes balance and calm.
       </p>
 
-      <div class="flex flex-row justify-between">
+      <!-- Value content -->
+      <div class="flex flex-row justify-between py-1">
         <div class="flex flex-row items-center gap-2">
           <img class="h-4" src="./assets/img/icon-ethereum.svg" alt="icon-ethereum">
           <p class="text-cyan whitespace-nowrap">0.041 ETH</p>
@@ -27,11 +37,12 @@
         </div>
       </div>
 
-
+      <!-- Creator callout -->
       <div class="flex flex-row gap-4 pt-4 border-t-2 border-dark-blue-line">
-          <img class="w-8 rounded-full border-2 border-white" src="./assets/img/image-avatar.png" alt="image-avatar">
-          <p class="text-soft-blue">Creation of <span class="text-white">Jules Wyvern</span></p>
-        </div>
+        <img class="w-8 rounded-full border-2 border-white" src="./assets/img/image-avatar.png" alt="image-avatar">
+        <p class="text-soft-blue">Creation of <span class="text-white hover:text-cyan hover:cursor-pointer">Jules
+            Wyvern</span></p>
+      </div>
     </div>
 
 
